f1c99e86dc9c8ce5ca5d7e217b6b4d2a88ae27fe
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-08-19 22:53:53 -0500

    feat/addAbilityToSendMessagesToServer

--- public/components/app.js
+++ public/components/app.js
@@ -1,14 +1,21 @@
 angular.module('how-do-i-sound').
-controller('AppCtrl', function($http) {
+controller('AppCtrl', function($http, $scope) {
   var that = this;
-  this.results = '';
-  var message = "Hi, my name is Sheena and I am testing out this api. I just completed Hack Reactor last week and I want to make sure that I'm keeping my skills sharp by using different types of technology.";
-  var params = {
-    text: message
-  };
+  this.results = [];
+  // var message = "Hi, my name is Sheena and I am testing out this api. I just completed Hack Reactor last week and I want to make sure that I'm keeping my skills sharp by using different types of technology.";
+  // var params = {
+  //   text: message
+  // };
   this.getData = function() {
+    this.results = [];
+    var params = {text: $scope.text};
+    $scope.text = '';
     $http.get('/apirequest', {params: params}).then(function(response) {
-      that.results = response.data;
+      var data = response.data.document_tone.tone_categories[0].tones;
+      console.log(data);
+      data.forEach(function(tone) {
+        that.results.push(`${tone.tone_name} = ${tone.score * 100}%`);
+      });
     });
   };
 })
